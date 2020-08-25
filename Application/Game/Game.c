3c947e706893f5cbddf0debc98e3832c1f3eb9d4
Robear Selwans <robear.selwans@outlook.com>
2020-08-25 20:24:18 +0200

    Added basic ScriptModule
    Signed-off-by: Robear Selwans <robear.selwans@outlook.com>

--- Application/Game/Game.c
+++ Application/Game/Game.c
@@ -1,6 +1,7 @@
 #include "Game.h"
 #include "World/World.h"
 #include "World/modules/transform_module.h"
+#include "World/modules/script_module.h"
 
 static int ev_game_init();
 static int ev_game_deinit();
@@ -16,6 +17,21 @@ struct ev_Game_Data {
 
 static int ev_game_init()
 {
+  ImportModule(TransformModule);
+  ImportModule(ScriptModule);
+
+  Entity cube = CreateEntity();
+
+  Entity_AddComponentSet(cube, Transform);
+  Entity_SetComponent(cube, ScriptComponent, {"path/to/script"});
+
+  ecs_set_target_fps(World.instance, 1);
+
+  printf("Components in cube: %s\n", Entity_PrintComponents(cube));
+  printf("Content of ScriptComponent in cube: %s\n", Entity_GetComponent(cube, ScriptComponent)->script_path);
+
+  while (ecs_progress(World.instance, 0));
+
   return 0;
 }
 
---
+++ Application/World/modules/script_module.c
@@ -0,0 +1,21 @@
+#include "script_module.h"
+
+void RunOnUpdate(ecs_iter_t *it)
+{
+  ECS_COLUMN(it, ScriptComponent, script, 1);
+
+  // Somehow run the OnUpdate function in the script
+  printf("Scriptname: %s, OnUpdate\n", script->script_path);
+}
+
+void ScriptModuleImport(ecs_world_t *world)
+{
+  ECS_MODULE(world, ScriptModule);
+
+  ECS_COMPONENT(world, ScriptComponent);
+
+  ECS_SYSTEM(world, RunOnUpdate, EcsOnUpdate, ScriptComponent);
+
+  ECS_EXPORT_COMPONENT(ScriptComponent);
+  ECS_EXPORT_ENTITY(RunOnUpdate);
+}
