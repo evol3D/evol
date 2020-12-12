0ae3cfb43029a46a2d51f1f7d32fa3fee0ef6af6
Robear Selwans <robear.selwans@outlook.com>
2020-12-12 11:45:04 +0200

    Added compiler specific directives and edited EV_ALIGN placement
    Signed-off-by: Robear Selwans <robear.selwans@outlook.com>

--- include/types.h
+++ include/types.h
@@ -1,14 +1,14 @@
 #ifndef EVOL_TYPES_H
 #define EVOL_TYPES_H
 
-#ifdef __GNUC__
+#ifdef EV_CC_GCC
 # define EV_UNUSED   __attribute__((unused))
 # define EV_ALIGN(x) __attribute__((aligned(x)))
 # define EV_PACKED   __attribute__((packed))
-#elif _MSC_BUILD
-# define EV_UNUSED   const // This is a hack with zero guarantees of actually working.
+#elif EV_CC_MSVC
+# define EV_UNUSED
 # define EV_ALIGN(x) __declspec(align(x))
-# define EV_PACKED   __pragma(pack(1))
+# define EV_PACKED
 #else
 # define EV_UNUSED
 # define EV_ALIGN(x)
@@ -21,18 +21,18 @@ typedef struct
 {
   real x, y, z;
   real padding;
-} EV_PACKED ev_Vector3 EV_ALIGN(16);
+} EV_PACKED EV_ALIGN(16) ev_Vector3;
 
-typedef real ev_Matrix4[4][4] EV_ALIGN(16);
+typedef real EV_ALIGN(16) ev_Matrix4[4][4];
 
 typedef struct
 {
   real x, y, z, w;
-} EV_PACKED ev_Vector4 EV_ALIGN(16);
+} EV_PACKED EV_ALIGN(16) ev_Vector4;
 
 typedef struct
 {
   real x, y;
-} EV_PACKED ev_Vector2 EV_ALIGN(16);
+} EV_PACKED EV_ALIGN(16) ev_Vector2;
 
 #endif
