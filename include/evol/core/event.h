97f4f716a6025daad95b9c1a14f24b160efe7533
Robear Selwans <robear.selwans@outlook.com>
2021-02-08 06:35:54 +0200

    Removed misleading attribute for event import
    Signed-off-by: Robear Selwans <robear.selwans@outlook.com>

--- include/evol/core/event.h
+++ include/evol/core/event.h
@@ -33,12 +33,12 @@ extern ev_eventtype_t SECONDARY_EVENT_TYPE_COUNT;
 
 #define EVENT_DECLARE_PRIMARY_IMPORT(T, ...)                                                \
   typedef struct T {ev_eventtype_t type; struct __VA_ARGS__;} T;               \
-  EV_IMPORT ev_eventtype_t EVENT_TYPE(T);                                       \
-  EV_IMPORT ev_eventtype_t EV_CONCAT(EVENT_TYPE(T), _CHILDCOUNT);
+  ev_eventtype_t EVENT_TYPE(T);                                       \
+  ev_eventtype_t EV_CONCAT(EVENT_TYPE(T), _CHILDCOUNT);
 
 #define EVENT_DECLARE_SECONDARY_IMPORT(P, T, ...)                                                \
   typedef struct T {struct P; struct __VA_ARGS__;} T;               \
-  EV_IMPORT ev_eventtype_t EVENT_TYPE(T);                                       \
+  ev_eventtype_t EVENT_TYPE(T);                                       \
 
 #define EVENT_DEFINE_PRIMARY(T, ...)                                                      \
   ev_eventtype_t EVENT_TYPE(T);                                              \
