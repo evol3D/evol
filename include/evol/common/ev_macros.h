688decdb19706db9d025507a2d259e1c75ef7564
Robear Selwans <robear.selwans@outlook.com>
2021-02-08 06:20:02 +0200

    Fixed typo in macro call
    Signed-off-by: Robear Selwans <robear.selwans@outlook.com>

--- include/evol/common/ev_macros.h
+++ include/evol/common/ev_macros.h
@@ -123,7 +123,7 @@
 #define EV_VA_ARGS_ARG_N(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, N, ...) N
 #define EV_VA_ARGS_RSEQ_N() 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
 
-#define EV_SUB_EACH_1 (I, ...) CHARCODE(I)
+#define EV_SUB_EACH_1 (I, ...) EV_CHARCODE(I)
 #define EV_SUB_EACH_2 (I, ...) EV_CONCAT(EV_CHARCODE(I), EV_SUB_EACH_1(__VA_ARGS__))
 #define EV_SUB_EACH_3 (I, ...) EV_CONCAT(EV_CHARCODE(I), EV_SUB_EACH_2(__VA_ARGS__))
 #define EV_SUB_EACH_4 (I, ...) EV_CONCAT(EV_CHARCODE(I), EV_SUB_EACH_3(__VA_ARGS__))
