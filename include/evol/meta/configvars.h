dd413a99f3660f8713b384211c89f2749544257e
Robear Selwans <robear.selwans@outlook.com>
2021-02-09 12:38:53 +0200

    Config variable changes
    * Removed the eventsystem update rate
    * Added version_major and version_minor
    
    Signed-off-by: Robear Selwans <robear.selwans@outlook.com>

--- include/evol/meta/configvars.h
+++ include/evol/meta/configvars.h
@@ -6,5 +6,5 @@
 static const char *CONFIGVAR_NAME  = "name";
 static const char *CONFIGVAR_MODDIR = "module_directory";
 static const char *CONFIGVAR_STARTMODS = "startmods";
-
-static const char *CONFIGVAR_EVENTSYSTEM_UPDATERATE = "eventsystem_rate";
+static const char *CONFIGVAR_VERSION_MAJOR = "version_major";
+static const char *CONFIGVAR_VERSION_MINOR = "version_minor";
