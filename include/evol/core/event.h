dfbaae41c35c907f3a31091498587a2d3fbc5108
Robear Selwans <robear.selwans@outlook.com>
2021-02-09 03:41:09 +0200

    Added extern to event declarations
    Signed-off-by: Robear Selwans <robear.selwans@outlook.com>

--- include/evol/core/event.h
+++ include/evol/core/event.h
@@ -24,12 +24,12 @@ EVCOREAPI extern ev_eventtype_t SECONDARY_EVENT_TYPE_COUNT;
 
 #define EVENT_DECLARE_PRIMARY_EXPORT(T, ...)                                                \
   typedef struct T {ev_eventtype_t type; struct __VA_ARGS__;} T;               \
-  EV_EXPORT ev_eventtype_t EVENT_TYPE(T);                                       \
-  EV_EXPORT ev_eventtype_t EV_CONCAT(EVENT_TYPE(T), _CHILDCOUNT);
+  EV_EXPORT extern ev_eventtype_t EVENT_TYPE(T);                                       \
+  EV_EXPORT extern ev_eventtype_t EV_CONCAT(EVENT_TYPE(T), _CHILDCOUNT);
 
 #define EVENT_DECLARE_SECONDARY_EXPORT(P, T, ...)                                                \
   typedef struct T {struct P; struct __VA_ARGS__;} T;               \
-  EV_EXPORT ev_eventtype_t EVENT_TYPE(T);
+  EV_EXPORT extern ev_eventtype_t EVENT_TYPE(T);
 
 #define EVENT_DECLARE_PRIMARY_IMPORT(T, ...)                                                \
   typedef struct T {ev_eventtype_t type; struct __VA_ARGS__;} T;               \
