11897182851626755690b0ce8966b039a90a75e4
Robear Selwans <robear.selwans@outlook.com>
2021-02-09 03:27:06 +0200

    Exporting log functions from core
    Signed-off-by: Robear Selwans <robear.selwans@outlook.com>

--- include/evol/common/ev_log.h
+++ include/evol/common/ev_log.h
@@ -11,6 +11,7 @@
 #include <stdarg.h>
 #include <stdbool.h>
 #include <time.h>
+#include <evol/common/ev_macros.h>
 
 #define LOG_VERSION "0.1.0"
 
@@ -36,18 +37,18 @@ enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
 #define ev_log_error(...) log_log(LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
 #define ev_log_fatal(...) log_log(LOG_FATAL, __FILE__, __LINE__, __VA_ARGS__)
 
-const char *
+EVCOREAPI const char *
 log_level_string(int level);
-void
+EVCOREAPI void
 log_set_lock(log_LockFn fn, void *udata);
-void
+EVCOREAPI void
 log_set_level(int level);
-void
+EVCOREAPI void
 log_set_quiet(bool enable);
-int
+EVCOREAPI int
 log_add_callback(log_LogFn fn, void *udata, int level);
-int
+EVCOREAPI int
 log_add_fp(FILE *fp, int level);
 
-void
+EVCOREAPI void
 log_log(int level, const char *file, int line, const char *fmt, ...);
